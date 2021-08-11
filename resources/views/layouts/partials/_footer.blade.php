c226fe3bf898ae6ed69d656266a3778a73e54b71
Paul Anthony McGowan <xhostcom@gmail.com>
2021-08-11 07:26:50 +0100

    First Commit

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,21 @@
+<footer class="text-center text-dark mt-4" style="background-color: #e8e8e8;">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: CTA -->
+      <section class="">
+        <p class="d-flex justify-content-center align-items-center">
+          <span class="me-3">Register on Apaci for free&nbsp;</span>
+           <button class="btn btn-primary btn-rounded">&nbsp;Sign Up</button>
+        </p>
+      </section>
+      <!-- Section: CTA -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2020 Copyright:
+      <a class="text-white" href="https://xhostcom.com/">Paul Anthony McGowan</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
